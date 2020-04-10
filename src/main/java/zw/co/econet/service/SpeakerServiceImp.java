c50521e9608d960ccef2273929d261e40e4a0c8e
Marvelous Mashamba <marvelousmashamba.mm@gmail.com>
2020-04-10 15:00:25 +0200

    Initial commit

---
+++ src/main/java/zw/co/econet/service/SpeakerServiceImp.java
@@ -0,0 +1,26 @@
+package zw.co.econet.service;
+
+import zw.co.econet.model.Speaker;
+import zw.co.econet.repository.SpeakerRepository;
+import zw.co.econet.repository.SpeakerRepositoryImp;
+
+import java.util.List;
+
+public class SpeakerServiceImp implements SpeakerService {
+
+    private SpeakerRepository repo ;
+
+
+    @Override
+    public List<Speaker> findAll(){
+        return repo.findSpeaker() ;
+    }
+
+    public void setRepo(SpeakerRepositoryImp repo) {
+        this.repo=repo;
+    }
+
+    public SpeakerServiceImp(SpeakerRepository repo) {
+        this.repo = repo;
+    }
+}
